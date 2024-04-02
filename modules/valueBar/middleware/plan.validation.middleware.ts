8d1437904b1f7ce862a77609d249ec4f709899e7
LuckyYoila <luckyyoila626@gmail.com>
2024-04-02 23:52:23 +0100

    setup subscription files, added to subscription routes, update plan validator

--- modules/valueBar/middleware/plan.validation.middleware.ts
+++ modules/valueBar/middleware/plan.validation.middleware.ts
@@ -25,6 +25,31 @@ class PlanValidationMiddleware {
         .json({ status: 'error', message: `${err.details[0].message}` });
     }
   }
+
+  async updatePlanValidator(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) {
+    const schema = Joi.object()
+      .keys({
+        name: Joi.string().required(),
+        description: Joi.string().required(),
+        amount: Joi.number().required(),
+        interval: Joi.string().required(),
+      })
+
+    try {
+      await schema.validateAsync(req.body);
+      return next();
+    }
+    catch (err: any) {
+      return res
+        .status(ServerResponseStatus.BAD_REQUEST)
+        .json({ status: 'error', message: `${err.details[0].message}` });
+    }
+  }
+  
 }
 
 export default new PlanValidationMiddleware();
